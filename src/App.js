d06180499822bead963c8d4955e8204f9f955e3e
woodtop <cwood89@mac.com>
2020-04-23 17:01:13 -0400

    adding zip functionality

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import Weather from './Components/weather';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import logo from "./images/logo512.png";
 
 export default function App() {
   return (
-    <Container className="p-3">
-      <Jumbotron className="card card-image gradient">
-        <Weather />
-      </Jumbotron>
-    </Container>
+    <>
+      <Container className="p-3">
+        <Jumbotron className="card card-image gradient pt-3">
+          <div className="d-flex">
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="The weather app logo"
+            />
+            <h3 className="px-3">The Weather App</h3>
+          </div>
+          <Weather />
+        </Jumbotron>
+      </Container>
+    </>
   );
 }
 
--- src/Components/weather.js
+++ src/Components/weather.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { getCurrentPosition, formatTemp, getTime } from "../utils";
-
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
 export default function Weather() {
   
   const [temp, setTemp] = useState("0 \xB0");
   const [city, setCity] = useState("City");
   const [description, setDescription] = useState("Description");
   const [time, setTime] = useState("0:00");
-
+  const [img, setImg] = useState()
+  const[showForm , setShowForm] = useState(false)
+  const imgURL = `http://openweathermap.org/img/wn/${img}@2x.png`
   const API_KEY = process.env.REACT_APP_API_KEY;
 
   useEffect(() => {
@@ -19,6 +22,7 @@ export default function Weather() {
        
       let data = await api_call.json()
       console.log(data);
+      setImg(data.weather[0].icon)
       setTemp(formatTemp(data.main.temp));
       setCity(data.name);
       setDescription(data.weather[0].description);
@@ -28,13 +32,43 @@ export default function Weather() {
     getLocation();
   }, [API_KEY]) // will only re-run if api key changes
 
+  const zipLocation = async (zip) => {
+    let api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?${zip}&appid=${API_KEY}`).catch(e => console.log("Error: ", e.message));
+       
+      let data = await api_call.json()
+      console.log(data);
+      setTemp(formatTemp(data.main.temp));
+      setCity(data.name);
+      setDescription(data.weather[0].description);
+      setTime(getTime(data.timezone));
+  }
+
+  const toggleForm = () => {
+    if (showForm === false) {
+      return (
+        <Button variant="primary" onClick={() => setShowForm(true)}>Pick a location!</Button>)
+    } else {
+      return (
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Enter a zip code!" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      )
+    }
+  }
 
   return (
-    <div className="text-white px-4">
-    <h1 className="header">{temp}</h1>
-    <p>{city}</p>
-    <p>{description}</p>
-    <p>{time}</p>
+    <div className=" text-uppercase text-white text-center px-4">
+      <img src={imgURL} alt={description}/>
+      <h1 className="header">{temp}</h1>
+      <p>{description}</p>
+      <p>{city}</p>
+      <p>{time}</p>
+      {toggleForm()}
     </div>
   )
 }
